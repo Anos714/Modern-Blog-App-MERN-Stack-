8092fe9e94e29578be72a23ba13bf98ab11f3942
Anos714 <rahul_sain0@outlook.com>
2026-01-10 17:15:46 +0530

    created user model

--- server/models/User.js
+++ server/models/User.js
@@ -2,10 +2,28 @@ import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema(
   {
-    username: {},
-    email: {},
-    password: {},
-    role: {},
+    username: {
+      type: String,
+      required: [true, "username is required"],
+      unique: [true, "username must be unique"],
+      trim: true,
+      minLength: [3, "username must be atleast 3 characters long"],
+    },
+    email: {
+      type: String,
+      required: [true, "email is required"],
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "password is required"],
+      trim: true,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
   },
   { timestamps: true }
 );
