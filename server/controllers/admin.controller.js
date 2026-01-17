1ddf067f0a8b4b0406ced56b732a11b042da0f83
Anos714 <rahul_sain0@outlook.com>
2026-01-17 18:30:52 +0530

    update all users function and add all comments controller function

--- server/controllers/admin.controller.js
+++ server/controllers/admin.controller.js
@@ -1,11 +1,38 @@
+import { CommentModel } from "../models/Comment.js";
 import { UserModel } from "../models/User.js";
 
 export const allUsers = async (req, res, next) => {
   try {
-    const users = await UserModel.find({}).select("-password");
+    const users = await UserModel.find({})
+      .select("-password")
+      .sort({ createdAt: -1 });
+    const totalUsersCount = await UserModel.find({}).countDocuments();
+    const adminUserCount = await UserModel.find({
+      role: "admin",
+    }).countDocuments();
     return res.status(200).json({
       success: true,
       users,
+      totalUsersCount,
+      adminUserCount,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const allComments = async (req, res, next) => {
+  try {
+    const comments = await CommentModel.find({})
+      .populate({
+        path: "userId",
+        select: "-password",
+      })
+      .populate("blogId")
+      .sort({ createdAt: -1 });
+    return res.status(200).json({
+      success: true,
+      comments,
     });
   } catch (error) {
     next(error);
--- server/routes/admin.route.js
+++ server/routes/admin.route.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { isAdmin } from "../middlewares/isAdmin.moddleware.js";
 import { isAuthenticated } from "../middlewares/isAuthenticated.middleware.js";
-import { allUsers } from "../controllers/admin.controller.js";
+import { allComments, allUsers } from "../controllers/admin.controller.js";
 
 const router = Router();
 
-router.get("/all", isAuthenticated, isAdmin, allUsers);
+router.get("/users", isAuthenticated, isAdmin, allUsers);
+router.get("/comments", isAuthenticated, isAdmin, allComments);
 
 export const adminRouter = router;
